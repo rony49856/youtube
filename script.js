aafb49e32f6ab5fc50d9d2953f05c9c0a6a67360
rony49856 <rahathossan71@gmail.com>
2025-08-07 14:04:53 +0600

    I love it

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function changeMessage() {
   document.getElementById("message").textContent = "You clicked the button!";
+  console.log("Messege has been changed");
 }
 
-console.log("Messege has been changed");
+console.log("The code is working great");
